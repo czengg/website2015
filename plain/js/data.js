d389770a6a4cab45442ac0a5e50c2800cf99818c
czengg <czeng67@gmail.com>
2015-12-09 19:13:52 -0800

    moved files into /plain folder

---
+++ plain/js/data.js
@@ -0,0 +1,137 @@
+var data = {
+  selectedProjects: [
+    {
+      sectionTitle: 'the intersection of design and code',
+      sectionHeader: 'user experience projects',
+      description: [
+        {
+          highlight: false,
+          text: 'Design and development go hand-in-hand. These projects exemplify the '
+        },
+        {
+          highlight: true,
+          text: 'hci'
+        },
+        {
+          highlight: false,
+          text: ' process of '
+        },
+        {
+          highlight: true,
+          text: 'research-based'
+        },
+        {
+          highlight: false,
+          text: ' design and development.'
+        }
+      ],
+      projects: [
+        {
+          type: 'company',
+          title: 'Zazzle',
+          skillType: 'languages',
+          skills: 'Javascript, C#',
+          dates: '2015',
+          desc: ''
+        },
+        {
+          type: 'startup',
+          title: 'Roaring Fork LLC',
+          skillType: 'frameworks',
+          skills: 'Angular Material',
+          dates: '2015',
+          desc: ''
+        },
+        {
+          type: 'nonprofit',
+          title: 'Pittsburgh Parks',
+          skillType: 'languages',
+          skills: 'Swift',
+          dates: '2015',
+          desc: ''
+        },
+        {
+          title: 'Zero Waste Pittsburgh',
+          type: 'nonprofit',
+          skillType: 'frameworks',
+          skills: 'Ruby on Rails',
+          dates: '2014',
+          desc: ''
+        }
+      ]
+    },
+    {
+      sectionTitle: 'selected code projects',
+      sectionHeader: 'coding projects',
+      projects: [
+        {
+          type: 'startup',
+          title: 'Canvs',
+          skillType: 'frameworks',
+          skills: 'AngularJS',
+          dates: '2014',
+          desc: ''
+        },
+        {
+          type: 'startup',
+          title: 'Drawbridge',
+          skillType: 'languages',
+          skills: 'Javascript, PHP',
+          dates: '2014',
+          desc: ''
+        },
+        {
+          type: 'project',
+          title: 'fig.',
+          skillType: 'languages',
+          skills: 'Python',
+          dates: '2013',
+          desc: ''
+        },
+        {
+          type: 'project',
+          title: 'Notetaker',
+          skillType: 'languages',
+          skills: 'Javascript',
+          dates: '2013',
+          desc: ''
+        }
+      ]
+    },
+    {
+      sectionTitle: 'selected design projects',
+      sectionHeader: 'design projects',
+      projects: [
+        {
+          type: 'company',
+          title: 'BNY Mellon',
+          skillType: 'programs',
+          skills: 'Axure',
+          dates: '2013',
+          desc: ''
+        },
+        {
+          type: 'company',
+          title: 'thelatest',
+          skillType: 'programs',
+          skills: 'Adobe Creative Suite',
+          desc: ''
+        }
+      ]
+    }
+  ],
+  contact: [
+    {
+      name: 'email',
+      url: ''
+    },
+    {
+      name: 'linkedin',
+      url: ''
+    },
+    {
+      name: 'twitter',
+      url: ''
+    }
+  ]
+};
---
+++ plain/js/scripts.js
@@ -0,0 +1,71 @@
+$(function() {
+
+  //render the scene
+  var selectedProjectsTemplate = Handlebars.compile($('#selected-projects').html());
+
+  //render
+  $('.selected-projects-content').html(selectedProjectsTemplate(data.selectedProjects));
+
+  //wire it all up
+  $(document).ready(function () {
+    var pagesections = $('.pagesection');
+    var containerTops = pagesections.map(function(i) {
+      var container = pagesections[i];
+      var obj = {};
+      obj['top'] = container.offsetTop;
+      obj['bottom'] = container.offsetTop + container.offsetHeight;
+      obj['heading'] = container.dataset.content;
+      return obj;
+    });
+    var header = $('#headerText');
+    var current = 0;
+    var lastScrollTop = 0;
+
+    var compareTops = function (a, b) {
+      if (a.top < b.top)
+        return -1;
+      if (a.top > b.top)
+        return 1;
+      return 0;
+    };
+
+    var changeHeader = function () {
+      var scrollTop = $(window).scrollTop();
+
+      if (scrollTop > lastScrollTop) {
+        var next;
+
+        while (scrollTop > containerTops[current].top) {
+          next = current + 1;
+
+          if (scrollTop < containerTops[current].bottom || current === containerTops.length)
+            break;
+
+          current = next;
+        }
+      } else {
+        var prev;
+
+        while (scrollTop < containerTops[current].bottom) {
+          prev = current - 1;
+
+          if (scrollTop > containerTops[current].top || current === 0)
+            break;
+
+          current = prev;
+        }
+      }
+
+      header.html(containerTops[current].heading.toUpperCase());
+      lastScrollTop = scrollTop;
+    };
+
+    containerTops.sort(compareTops);
+    changeHeader();
+
+    $(window).scroll(function () {
+      changeHeader();
+    });
+  });
+
+});
